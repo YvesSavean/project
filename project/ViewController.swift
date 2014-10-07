c2a372a7768d7a06e1860b3b234d6a5ea7189388
YvesSavean <savean@mbp-de-savean.home>
2014-10-07 13:08:34 +0200

    mis en place de plusieurs mode de calcul

--- project/ViewController.swift
+++ project/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var lblNum1: UILabel!
+    @IBOutlet var lblSign: UILabel!
+    @IBOutlet var lblNum2: UILabel!
+    @IBOutlet var txtReponse: UITextView!
+    var results = 0
+    var tab = ["+","-","*","/"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Rand()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func Rand(){
+        let num1 = Int(arc4random_uniform(10))
+        let num2 = Int(arc4random_uniform(10))
+        let valeurSign = Int(arc4random_uniform(4))
+        lblNum1.text = "\(num1)"
+        lblNum2.text = "\(num2)"
+        lblSign.text = "\(tab[valeurSign])"
+        if valeurSign == 0 {
+            results = num1 + num2
+        }
+        if valeurSign == 1 {
+            results = num1 - num2
+        }
+        if valeurSign == 2 {
+            results = num1 * num2
+        }
+        if valeurSign == 3 {
+            results = num1 / num2
+        }
+        
+    }
 
+    @IBAction func valider(sender: AnyObject) {
+        if txtReponse.text.toInt() == results {
+            Rand()
+        }
+        else{
+            txtReponse.text = "false"
+        }
+    }
 
 }
 
